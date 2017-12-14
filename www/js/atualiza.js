c496476cf3a2f85bcb0839b7b62e2cdaed230ea8
Robson <robson@popcomputadores.com.br>
2017-12-14 13:21:32 -0200

    ...

--- www/js/atualiza.js
+++ www/js/atualiza.js
@@ -10,13 +10,9 @@ function AtualizarApp(){
 
 
 	if(device.platform=="Android"){
-	// cordova.InAppBrowser.open('market://details?id=app.pandapizza.cliente', '_system', 'location=yes');
-  //  window.location.replace("market://details?id=app.pandapizza.cliente");
 		cordova.plugins.market.open(link_google);
   }else {
 		cordova.plugins.market.open(link_apple);
-		// cordova.InAppBrowser.open('itms://itunes.apple.com/br/app/masterhub/id1143525346', '_system', 'location=yes');
-  //  window.location.replace("itms://itunes.apple.com/br/app/masterhub/id1143525346");
   }
 
 }
--- www/js/share.js
+++ www/js/share.js
@@ -7,10 +7,9 @@ function whatsappShare(){
 		link_google_play = getStorage("link_google_play");
 	    dump("link_google_play=>"+link_google_play);
 	
-	window.plugins.socialsharing.shareViaWhatsApp('Eu pedi online e entregaram mais rápido que por telefone. Baixe o aplicativo da Playstore. ', 
-                                                 null /* img */, 
-                                                 "https://play.google.com/store/apps/details?id="+link_google+"&hl=pt-BR", 
-                                                 null, 
-                                                 function(errormsg){alert("Erro: Não foi compartilhado")}
-                                                 );
+	window.plugins.socialsharing.shareViaWhatsApp('Eu pedi online e entregaram mais rápido que por telefone. Baixe o aplicativo da Playstore. ', null /* img */,
+												  
+     "https://play.google.com/store/apps/details?id="+link_google+"&hl=pt-BR", null, 
+              function(errormsg){alert("Erro: Não foi compartilhado")}
+         );
 };
