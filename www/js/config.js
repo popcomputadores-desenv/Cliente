f8a2930992c16bb750b79170b37e5f8ebed575be
Robson Goncalves <robson@popcomputadores.com.br>
2018-11-05 14:35:40 -0300

    ...

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={				
 	'ApiUrl':"http://integracao.masterhub.com.br/mobileapp/api",	
-	'DialogDefaultTitle':"Master Hub",	
+	'DialogDefaultTitle':"Master Hub®",	
 	'APIHasKey':"f0b8922f6edcb9d46f7f33e867e70e7d",
 	'debug': false
 };
