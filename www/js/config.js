5e711938a42e89f2a6c3aad75cc297c1dfe675b0
Robson Goncalves <robson@popcomputadores.com.br>
2018-12-29 03:41:43 -0200

    first push

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={				
+	'ApiUrl':"http://piloto.masterhub.com.br/mobileapp/api",	
+	'DialogDefaultTitle':"Master Hub®",	
+	'APIHasKey':"f0b8922f6edcb9d46f7f33e867e70e7d",
+	'debug': true
+};
