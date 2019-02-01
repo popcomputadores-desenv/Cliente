09c4eaac612fc36252ceb483011e6291d51de426
Robson Gonçalves <robson@masterhub.com.br>
2019-02-01 16:07:58 -0200

    primeiro init

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+var krms_config ={				
+	'ApiUrl':"http://piloto.masterhub.com.br/mobileapp/api",
+	'Website':"http://piloto.masterhub.com.br",	
+	'UploadUrl':"http://piloto.masterhub.com.br/upload/",
+	'DialogDefaultTitle':"Master Hub®",	
+	'APIHasKey':"f0b8922f6edcb9d46f7f33e867e70e7d",
+	'debug': false
+};
