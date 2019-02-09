300d30b07382f4de5ec322a2be4c385a567a4f9f
Robson Gonçalves <robson@masterhub.com.br>
2019-02-09 13:58:40 -0200

    ...

--- www/js/config.js
+++ www/js/config.js
@@ -3,6 +3,6 @@ var krms_config ={
 	'Website':"http://piloto.masterhub.com.br",	
 	'UploadUrl':"http://piloto.masterhub.com.br/upload/",
 	'DialogDefaultTitle':"Master Hub®",	
-	'APIHasKey':"f0b8922f6edcb9d46f7f33e867e70e7d",
+	'APIHasKey':"4m1DDci5rKsyeNlmkwZlSsQDCpg=",
 	'debug': false
 };
