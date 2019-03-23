b1534b2a4204abd2481d667c66ac8a5b42224ab8
Robson Gonçalves <robson@masterhub.com.br>
2019-03-22 22:05:40 -0300

    ...

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+var krms_config ={				
+	'ApiUrl':"http://app.masterhub.com.br/mobileapp/api",
+	'Website':"http://app.masterhub.com.br",	
+	'UploadUrl':"http://app.masterhub.com.br/upload/",
+	'DialogDefaultTitle':"Master Hub®",	
+	'APIHasKey':"4m1DDci5rKsyeNlmkwZlSsQDCpg=",
+	'debug': false
+};
