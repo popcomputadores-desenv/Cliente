fd54257f52b81bcfbb09ae2639fbd0339eff7caa
Robson <robson@masterhub.com.br>
2020-01-13 18:29:56 -0300

    ...

--- www/js/config.js
+++ www/js/config.js
@@ -2,5 +2,5 @@ var krms_config ={
 	'ApiUrl':"https://app.masterhub.com.br/mobileappv2/api",
 	'AppTitle':"Master Hub®",
 	'ApiKey' : '4m1DDci5rKsyeNlmkwZlSsQDCpg=',
-	'debug': true
+	'debug': false
 };
