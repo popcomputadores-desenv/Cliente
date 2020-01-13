7846ab4a67487a28dfb40aff22a2c7a9fa48216f
Robson <webmaster@masterhub.com.br>
2020-01-12 22:11:20 -0300

    ...

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={				
-	'ApiUrl':"",	
-	'AppTitle':"",
-	'ApiKey' : '',	
+	'ApiUrl':"https://piloto.masterhub.com.br/mobileappv2/api",	
+	'AppTitle':"Master Hub®",
+	'ApiKey' : '4m1DDci5rKsyeNlmkwZlSsQDCpg=',	
 	'debug': false
 };
